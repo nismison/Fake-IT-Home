a60e774cb335f33984031e500fdbf79ca6ae20d5
Nismison <nismison@gmail.com>
2021-02-24 14:05:08 +0800

    调整 骨架屏文件位置

--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -2,6 +2,6 @@ export 'custom_tab_indicator.dart';
 export 'default_tab_bar.dart';
 export 'image_cache.dart';
 export 'loading.dart';
-export '../pages/news/skeleton.dart';
+export '../pages/news_detail/skeleton.dart';
 export 'skeleton/skeleton_line.dart';
 export 'skeleton/skeleton_block.dart';
